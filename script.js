f21a9119a222ca6e6893770486b69faf7390fc10
megane <megane.londono@hotmail.com>
2020-05-06 15:08:04 +0200

    graphics added to page

---
+++ script.js
@@ -0,0 +1,81 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", init);
+
+const url = "https://kea-alt-del.dk/kata-distortion/";
+
+function init() {
+  console.log("one, two, three: ready");
+  fetchData();
+  fetchSVG();
+}
+
+function fetchSVG() {
+  fetch("svg/gauge.svg", {
+    method: "get",
+  })
+    .then((e) => e.text())
+    .then((e) => {
+      document.querySelector("#gauge").innerHTML = e;
+    });
+}
+
+function fetchData() {
+  fetch(url, {
+    method: "get",
+  })
+    .then((e) => e.json())
+    .then((e) => {
+      console.log(e);
+      showData(e);
+      setTimeout(fetchData, 5000);
+    });
+}
+
+function showData(data) {
+  const inQueue = data.inQueue;
+  document
+    .querySelector("#speedometer")
+    .style.setProperty("--deg", inQueue * 6 + "deg");
+  const bar = document.createElement("div");
+  const pTag = document.createElement("p");
+
+  pTag.textContent = inQueue;
+
+  bar.appendChild(pTag);
+
+  bar.className = "bar";
+  bar.style.width = inQueue * 8 + "px";
+
+  if (inQueue < 5) {
+    bar.style.background = "limegreen";
+  } else if (inQueue > 5 && inQueue < 15) {
+    bar.style.background = "yellow";
+    pTag.style.color = "black";
+  } else {
+    bar.style.background = "red";
+  }
+
+  document
+    .querySelector("#sidebar")
+    .insertBefore(bar, document.querySelector("#sidebar").firstChild);
+
+  document.querySelector(
+    "#info p+p"
+  ).textContent = `Queue updated at: ${data.loggedAt.substring(11)}`;
+
+  document.querySelectorAll("#speeder > g:nth-child(1) text").forEach((t) => {
+    if (t.textContent === inQueue) {
+      console.log(t.textContent);
+
+      if (inQueue < 5) {
+        t.style.fill = "limegreen";
+      } else if (inQueue > 5 && inQueue < 15) {
+        t.style.fill = "yellow";
+      } else {
+        t.style.fill = "red";
+      }
+    } else {
+      t.style.fill = "white";
+    }
+  });
+}
