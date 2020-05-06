c777698dfa59fbb3f20fcf416d13139d3f82414c
megane <megane.londono@hotmail.com>
2020-05-06 16:39:45 +0200

    data visualisation with emojis and numbers

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", fetchData);
 
 const url = "https://kea-alt-del.dk/kata-distortion/";
-
-function init() {
-  fetchData();
-  fetchSVG();
-}
-
-function fetchSVG() {
-  fetch("svg/gauge.svg")
-    .then((e) => e.text())
-    .then((e) => {
-      document.querySelector("#gauge").innerHTML = e;
-    });
-}
 
 function fetchData() {
   fetch(url, {
@@ -29,51 +16,12 @@ function fetchData() {
 }
 
 function showData(data) {
-  /* const inQueue = data.inQueue;
-  document
-    .querySelector("#gauge")
-    .style.setProperty("45deg", inQueue * 6 + "deg");
-  const bar = document.createElement("div");
-  const pTag = document.createElement("p");
-
-  pTag.textContent = inQueue;
-
-  bar.appendChild(pTag);
-
-  bar.className = "bar";
-  bar.style.width = inQueue * 8 + "px";
-
-  if (inQueue < 5) {
-    bar.style.background = "limegreen";
-  } else if (inQueue > 5 && inQueue < 15) {
-    bar.style.background = "yellow";
-    pTag.style.color = "black";
-  } else {
-    bar.style.background = "red";
-  } */
-
   // fetch real-time numbers to be displayed on page
   document.querySelector(
-    "#info p+p"
+    "#info p"
   ).textContent = `Queue updated at: ${data.loggedAt.substring(11)}`;
 
-  document.querySelector(
-    "#info p"
-  ).textContent = `Currently in queue: ${data.inQueue}`;
+  document.querySelector("#info p+p").textContent = `Currently in queue: `;
 
-  document.querySelectorAll("#gauge > g:nth-child(1) text").forEach((t) => {
-    if (t.textContent === inQueue) {
-      console.log(t.textContent);
-
-      if (inQueue < 5) {
-        t.style.fill = "limegreen";
-      } else if (inQueue > 5 && inQueue < 15) {
-        t.style.fill = "yellow";
-      } else {
-        t.style.fill = "red";
-      }
-    } else {
-      t.style.fill = "white";
-    }
-  });
+  document.querySelector("#chart p").textContent = `${data.inQueue}`;
 }
